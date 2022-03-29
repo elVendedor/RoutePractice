6766f4b06c5c8bd0b761a53e3cfb2d1ed49cbbc0
elVendedor <jesusmanuel170@gmail.com>
2022-03-29 12:27:11 -0300

    initial commit

---
+++ router-tutorial/src/App.jsx
@@ -0,0 +1,20 @@
+import { Outlet,Link } from "react-router-dom";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Bookeeper!</h1>
+
+      <nav
+        style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem",
+        }}
+      >
+        <Link to="/invoices">Invoices</Link> |{" "}
+        <Link to="/expenses">Expenses</Link>
+      </nav>
+      <Outlet/>
+    </div>
+  );
+}
---
+++ router-tutorial/src/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Expenses from "./routes/expenses";
+import Invoice from "./routes/invoice";
+import Invoices from "./routes/invoices";
+
+ReactDOM.render(
+  <BrowserRouter>
+    <React.StrictMode>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route path="expenses" element={<Expenses />} />
+          <Route path="invoices" element={<Invoices />} >
+          <Route path=":invoiceId" element={<Invoice/>}/>
+          </Route>
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There´s nothing here!</p>
+              </main>
+            }
+          />
+        </Route>
+      </Routes>
+    </React.StrictMode>
+  </BrowserRouter>,
+  document.getElementById("root")
+);
